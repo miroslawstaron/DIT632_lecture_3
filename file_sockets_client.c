37d939f11beb5033e43a2964b7c458dc2193cc1b
Miroslaw Staron <miroslaw.staron@live.se>
2022-01-31 08:56:15 +0100

    Minor changes - comments in client code

--- file_sockets_client.c
+++ file_sockets_client.c
@@ -20,23 +20,27 @@ int main_client(void)
 {
     WSADATA mywsadata; //your wsadata struct, it will be filled by WSAStartup
     
+    // WSAStartup function provides our program with the details
+    // of how sockets are implemented on our particular version of windows
+    // i.e. it sets up runtime parameters of the connection
     if (WSAStartup(MAKEWORD(2, 2), &mywsadata) != 0)
     {
-        printf("Failed. Error Code : %d", WSAGetLastError());
+        printf("Failed set-up of WSA Sockets. Error Code : %d", WSAGetLastError());
         return 1;
     }
 
-    int iSocket;
-    struct sockaddr_in server_addr;
-    char server_message[2000], client_message[2000];
+    int iSocket;                                        // Socket number
+    struct sockaddr_in server_addr;                     // address of the server
+    char server_message[2000], client_message[2000];    // strings which we will send/receive
 
-    // Clean buffers:
+    // Clean buffers, i.e. zero the strings
     memset(server_message, '\0', sizeof(server_message));
     memset(client_message, '\0', sizeof(client_message));
 
     // Create socket:
     iSocket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // failsafety
     if (iSocket < 0) {
         printf("Unable to create socket\n");
         return -1;
@@ -45,22 +49,25 @@ int main_client(void)
     printf("Socket created successfully\n");
 
     // Set port and IP the same as server-side:
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_family = AF_INET;                       // TCP connection
+    server_addr.sin_port = htons(PORT);                     // port
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");   // address == localhost
 
     // Send connection request to server:
     if (connect(iSocket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         printf("Unable to connect\n");
         return -1;
     }
+
     printf("Connected with server successfully\n");
 
-    // Get input from the user:
+    // Get what we want to send to the server
     printf("Enter message: ");
     gets(client_message);
 
-    // Send the message to server:
+    // Send the message to server
+    // please note that we need to specify how many bytes we need to send
+    // hence strlen
     if (send(iSocket, client_message, strlen(client_message), 0) < 0) {
         printf("Unable to send message\n");
         return -1;
